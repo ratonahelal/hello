c574a2e828b7fc61e851e9b9c91a5c28feaff505
ratonahelal <ratonahelal@gmail.com>
2024-12-11 22:41:53 -0500

    change

---
+++ eclipse-Upskill_Practise/Java_practcise/src/interfaceDamo/Cat.java
@@ -0,0 +1,9 @@
+package interfaceDamo;
+
+public class Cat implements Animal {
+	
+	public void eat() {
+		System.out.println("Cat drink milk");
+	}
+
+}
---
+++ eclipse-Upskill_Practise/Java_practcise/src/oop/Sharmin.java
@@ -0,0 +1,10 @@
+package oop;
+
+public class Sharmin extends mobileUser {
+	
+	void SendMessage() {
+		
+		System.out.println("Hi this is sharmin apu");
+	}
+
+}
